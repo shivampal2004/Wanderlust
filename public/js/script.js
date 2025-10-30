69819ee294016e76d69027f57e09ca81b5afe0e4
Shivam Kumar <shivampal2004@gmail.com>
2025-10-30 21:50:28 +0530

    Added Initial Map

--- public/js/script.js
+++ public/js/script.js
@@ -18,3 +18,23 @@
       }, false);
     });
 })();
+
+// MAP
+var map = L.map('map').setView([28.6139, 77.2088], 11);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; Wanderlust Private Limited'
+    }).addTo(map);
+
+// TAX-SWITCH
+let taxSwitch= document.getElementById("switchCheckReverse");
+taxSwitch.addEventListener("click", ()=>{
+    let taxInfo= document.getElementsByClassName("tax-info");
+    for(info of taxInfo){
+        if(info.style.display != "inline"){
+            info.style.display = "inline";
+        } else {
+            info.style.display = "none";
+        }
+    }
+});
